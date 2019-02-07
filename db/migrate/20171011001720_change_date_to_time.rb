ad307b74ef04e52aa06e8e540587b01f626b40dc
wholesomewilson <wilsonwan88@gmail.com>
2019-02-08 01:05:18 +0800

    init

--- db/migrate/20171011001720_change_date_to_time.rb
+++ db/migrate/20171011001720_change_date_to_time.rb
@@ -1,7 +1,5 @@
 class ChangeDateToTime < ActiveRecord::Migration
   def change
-    remove_column :lessons, :start_date, :time
-    remove_column :lessons, :end_date, :time
     add_column :lessons, :start_date, :date
     add_column :lessons, :end_date, :date
   end
