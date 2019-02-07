55c5620cab25c426d51cbfdb9d168ac9953bca47
wholesomewilson <wilsonwan88@gmail.com>
2019-02-08 00:35:56 +0800

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Change mailer method to :smtp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['M_URNE'],
+   :password             => ENV['M_PD'],
+   :authentication       => "plain",
+   :enable_starttls_auto => true
+  }
+
+  config.active_job.queue_adapter = :delayed_job
 end
