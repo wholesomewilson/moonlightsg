d59976b91961c2bc7e21810673afa9c02eee9d5c
wholesomewilson <wilsonwan88@gmail.com>
2019-02-16 01:35:28 +0800

    init

--- app/models/chatimage.rb
+++ app/models/chatimage.rb
@@ -1,6 +1,4 @@
 class Chatimage < ActiveRecord::Base
   belongs_to :message
   mount_uploader :image, ChatimageUploader
-  validates_integrity_of  :image
-  validates_processing_of :image
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,4 @@
 class Photo < ActiveRecord::Base
   belongs_to :lesson
   mount_uploader :image, JobPhotoUploader
-  validates_integrity_of  :image
-  validates_processing_of :image
 end
