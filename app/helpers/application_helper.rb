706b0b364cf9697f2231d11ad2aad102d6d524a4
wholesomewilson <wilsonwan88@gmail.com>
2019-03-08 13:00:49 +0800

    init

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,30 @@ module ApplicationHelper
       concat submit_tag title, class:"sorttag"
     end
   end
+
+  JS_ESCAPE_MAP = {
+    '\\'   => '\\\\',
+    '<'    => '\\u003c',
+    '&'    => '\\u0026',
+    '>'    => '\\u003e',
+    "\r\n" => '\n',
+    "\n"   => '\n',
+    "\r"   => '\n',
+    '"'    => '\\u0022',
+    "'"    => "\\u0027"
+  }
+
+  JS_ESCAPE_MAP["\342\200\250".force_encoding(Encoding::UTF_8).encode!] = '&#x2028;'
+  JS_ESCAPE_MAP["\342\200\251".force_encoding(Encoding::UTF_8).encode!] = '&#x2029;'
+
+  def escape_javascript_with_inside_html(javascript)
+    if javascript
+      result = javascript.gsub(/(\\|\r\n|\342\200\250|\342\200\251|[\n\r<>&"'])/u) {|match| JS_ESCAPE_MAP[match] }
+      javascript.html_safe? ? result.html_safe : result
+    else
+      ''
+    end
+  end
+
+  alias_method :jh, :escape_javascript_with_inside_html
 end
