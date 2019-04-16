fa8cb7babe4c82f57e0c121ef98758ec594c793e
wholesomewilson <wilsonwan88@gmail.com>
2019-04-16 16:14:33 +0800

    init

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,7 +4,9 @@ class ConversationsController < ApplicationController
     @conversation = Conversation.find(params[:conversation_id])
     if @conversation.read_status != current_user.id
       @conversation.update_attribute(:read_status, 0)
-      Delayed::Job.find(@conversation.message_notification_job_id).destroy
+      if @conversation.message_notification_job_id.present?
+        Delayed::Job.find(@conversation.message_notification_job_id).destroy
+      end
     end
     render :nothing => true
   end
