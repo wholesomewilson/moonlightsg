11f9754f8d00e01a7df80cd81c377137bec170d9
wholesomewilson <wilsonwan88@gmail.com>
2019-05-03 17:51:05 +0800

    init

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -11,17 +11,13 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
   # GET /resource/confirmation?confirmation_token=abcdef
   def show
-    if current_user.confirmed?
-      redirect_to lessons_path
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    if resource.errors.empty?
+        set_flash_message(:notice, :confirmed) if is_navigational_format?
+        sign_in(resource_name, resource)
+        respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
     else
-      self.resource = resource_class.confirm_by_token(params[:confirmation_token])
-      if resource.errors.empty?
-          set_flash_message(:notice, :confirmed) if is_navigational_format?
-          sign_in(resource_name, resource)
-          respond_with_navigational(resource){ redirect_to after_confirmation_path_for(resource_name, resource) }
-      else
-          respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
-      end
+        respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
     end
   end
 
