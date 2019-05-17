4eb5d1182b3e2aad879c74d426cdcc7c4e8bbe5a
wholesomewilson <wilsonwan88@gmail.com>
2019-05-18 03:23:36 +0800

    init

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -178,7 +178,7 @@ end
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
-    about_yourself_path
+    items_path
   end
 
   # The path used after sign up for inactive accounts.
