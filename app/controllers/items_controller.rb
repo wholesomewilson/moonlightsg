c989c594a9892ad6e3382c68f536c9ff0d021a90
wholesomewilson <wilsonwan88@gmail.com>
2019-06-07 16:08:17 +0800

    init

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,8 +5,7 @@ class ItemsController < ApplicationController
   def index
     @countdown = DateTime.parse("#{'12-06-2019'} #{'00'}:#{'00'}#{'AM'}")
     @items = Item.where(status: nil).where.not(brand: "Eureka").where.not(brand: "Merries").where.not(brand: "Drypers").where.not(brand: "Huggies").where.not(brand: "Famous Amos").where.not(brand: "Moist Diane Botanical").where.not(brand: "Moist Diane Extra").where.not(brand: "Downy").where.not(brand: "Lifebuoy").where.not(brand: "Head & Shoulders").where.not(brand: "Old Town").where.not(brand: "MamyPoko").sort_by { |x| x.brand }
-    @popcorn_6 = Item.where(status: nil).where(brand: "Eureka").where(price_my: 6.7).first
-    @popcorn_7 = Item.where(status: nil).where(brand: "Eureka").where(price_my: 7.5)
+    @popcorn_6 = Item.where(status: nil).where(brand: "Eureka").first
     @merries = Item.where(status: nil).where(brand: "Merries").first
     @drypers = Item.where(status: nil).where(brand: "Drypers").first
     @huggies = Item.where(status: nil).where(brand: "Huggies").first
@@ -23,8 +22,7 @@ class ItemsController < ApplicationController
   def show
     @countdown = DateTime.parse("#{'12-06-2019'} #{'00'}:#{'00'}#{'AM'}")
     @orderitem = @item.orderitems.build
-    @eureka_6 = Item.where(status: nil).where(brand: "Eureka").where(price_my: 6.7).sort_by{ |x| x.name }
-    @eureka_7 = Item.where(status: nil).where(brand: "Eureka").where(price_my: 7.5).sort_by{ |x| x.name }
+    @eureka_6 = Item.where(status: nil).where(brand: "Eureka").sort_by{ |x| x.name }
     @merries = Item.where(status: nil).where(brand: "Merries").sort_by{ |x| x.id }
     @drypers = Item.where(status: nil).where(brand: "Drypers").sort_by{ |x| x.id }
     @huggies = Item.where(status: nil).where(brand: "Huggies").sort_by{ |x| x.id }
